57719334e0bc54916ba371ca9ed4b29a9dab0edd
Gustavo Carvalho <gustavo1net@hotmail.com>
2021-10-31 16:34:54 -0300

    Norma map

--- utils/map.c
+++ utils/map.c
@@ -6,14 +6,14 @@
 /*   By: glima-de <glima-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 14:32:30 by glima-de          #+#    #+#             */
-/*   Updated: 2021/10/31 16:19:20 by glima-de         ###   ########.fr       */
+/*   Updated: 2021/10/31 16:33:55 by glima-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 #include "../libft/libft.h"
 
-static int	valid_only_one(t_game game, char c)
+static int	valid_only_one(t_game game, char c, char *err)
 {
 	int	x;
 	int	y;
@@ -29,7 +29,10 @@ static int	valid_only_one(t_game game, char c)
 			if (game.map[y][x] == c)
 			{
 				if (count > 0)
+				{
+					printf("%s\n", err);
 					return (0);
+				}
 				count++;
 			}
 			x++;
@@ -43,24 +46,28 @@ static int	valid_wall(t_game game)
 {
 	int	x;
 	int	y;
+	int	ok;
 
 	y = 0;
-	while (y < game.size.y)
+	ok = 1;
+	while (y < game.size.y && ok)
 	{
 		x = 0;
-		while (x < game.size.x)
+		while (x < game.size.x && ok)
 		{
 			if (x == 0 || y == 0)
 				if (game.map[y][x] != '1')
-					return (0);
+					ok = 0;
 			if (x == game.size.x - 1 || y == game.size.y - 1)
 				if (game.map[y][x] != '1')
-					return (0);
+					ok = 0;
 			x++;
 		}
 		y++;
 	}
-	return (1);
+	if (!ok)
+		printf("Error: It's necessary to have a wall surrounding the room\n");
+	return (ok);
 }
 
 static int	valid_map_size(t_game game)
@@ -71,7 +78,10 @@ static int	valid_map_size(t_game game)
 	while (y < game.size.y)
 	{
 		if (ft_strlen(game.map[y]) != (unsigned int)game.size.x)
+		{
+			printf("Error: Length of lines is not equals\n");
 			return (0);
+		}
 		y++;
 	}
 	return (1);
@@ -80,25 +90,13 @@ static int	valid_map_size(t_game game)
 int	check_valid_map(t_game game)
 {
 	if (!valid_map_size(game))
-	{
-		printf("Error: Length of lines is not equals\n");
 		return (0);
-	}
-	if (!valid_only_one(game, 'P'))
-	{
-		printf("Error: It's necessary only one player\n");
+	if (!valid_only_one(game, 'P', "Error: It's necessary only one player"))
 		return (0);
-	}
 	if (!valid_wall(game))
-	{
-		printf("Error: It's necessary to have a wall surrounding the room\n");
 		return (0);
-	}
-	if (!valid_only_one(game, 'E'))
-	{
-		printf("Error: It's necessary only one exit\n");
+	if (!valid_only_one(game, 'E', "Error: It's necessary only one exit\n"))
 		return (0);
-	}
 	if (!count_coins(game))
 	{
 		printf("Error: It's necessary one or more coins\n");
